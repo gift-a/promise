7546d1bf5e55955c58ee6eb8f432ba54cda57ad4
gift-a <ksenia.gift@gmail.com>
2018-03-21 10:54:42 +0200

    promise chain

--- test-ajax.js
+++ test-ajax.js
@@ -1,43 +1,41 @@
 console.log("let's get started");
 
-const dbRef = firebase.database();
-console.log(dbRef);
-
 const input = document.getElementById("input");
 const uploader = document.getElementById("uploader");
 
-input.addEventListener("change", e => {
-  const file = e.target.files[0];
-  console.log(firebase);
-  let storageRef = firebase.storage().ref(`files/${file.name}`);
-  let task = storageRef.put(file);
-
-  task.on(
-    "state_changed",
-    function progress(snapshot) {
-      let persentage = snapshot.bytesTransferred / snapshot.totalBytes * 100;
-      uploader.value = persentage;
-    },
-    function error(err) {},
-    function complete() {}
-  );
-});
-
-let storageRef = firebase.storage().ref("files/countries.txt");
-storageRef
-  .getDownloadURL()
-  .then(function(url) {
-    console.log(url);
-    return httpGet(url);
-  })
+loadFile()
+  .then(e => putFile(e))
+  .then(storageRef => storageRef.getDownloadURL())
+  .then(url => httpGet(url))
   .then(response => console.log(response));
 
-// function loadFile() {
-//   return new Promise(resolve => {
-//     const btn = document.getElementById("btn");
-//     btn.addEventListener("click", () => resolve());
-//   });
-// }
+function loadFile() {
+  return new Promise((resolve, reject) => {
+    input.addEventListener("change", e => resolve(e));
+  });
+}
+
+function putFile(e) {
+  return new Promise((resolve, reject) => {
+    const file = e.target.files[0];
+    let storageRef = firebase.storage().ref(`files/${file.name}`);
+    let task = storageRef.put(file);
+
+    task.on(
+      "state_changed",
+      function progress(snapshot) {
+        let persentage = snapshot.bytesTransferred / snapshot.totalBytes * 100;
+        uploader.value = persentage;
+      },
+      function error(err) {
+        reject(err);
+      },
+      function complete() {
+        resolve(storageRef);
+      }
+    );
+  });
+}
 
 function httpGet(url) {
   return new Promise(function(resolve, reject) {
@@ -61,8 +59,3 @@ function httpGet(url) {
     xhr.send();
   });
 }
-
-// loadFile()
-//   .then(() => httpGet(""))
-//   .then(response => console.log(response))
-//   .catch(e => console.error(e));
